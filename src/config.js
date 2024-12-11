edd1aef558297e69447b5fa3ea46772c460f8011
luotm <shadowai5200@gmail.com>
2024-12-11 18:55:38 +0800

    fix: 修改地址

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API: 'http://ws.juggleim.com/jim/',
+  API: 'https://ws.juggleim.com/jim/',
   appkey: 'nsw3sue72begyv7y',
   navList: ['https://nav.juggleim.com'],
   rtcAppId: 1881186044,
