991235df8de8787a8c40a835afd4701bcd69b389
luotm <shadowai5200@gmail.com>
2025-01-07 17:02:47 +0800

    feat: common asider

---
+++ src/components/aside.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive, watch, getCurrentInstance } from "vue";
+import utils from "../common/utils";
+import common from "../common/common";
+import emitter from "../common/emmit";
+
+const props = defineProps(["isShow"]);
+const emit = defineEmits([]);
+
+const context = getCurrentInstance();
+
+let state = reactive({
+  list: []
+});
+
+watch(() => props.isShow, () => {
+});
+</script>
+
+<template>
+  <div class="tyn-common-aside" :class="[props.isShow ? 'show-caside' : '']">
+  123123
+  </div>
+</template>
